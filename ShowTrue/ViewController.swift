025de4bbf8a7bff02231ab8b5102eae4c62c5f64
Kuznetcov Igor <kuznetcov28@mail.ru>
2023-04-12 11:10:37 +0300

    setup picture

--- ShowTrue/ViewController.swift
+++ ShowTrue/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var tomaLabel: UILabel!
+
+    @IBOutlet var showTrueButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tomaLabel.isHidden = true
+        showTrueButton.layer.cornerRadius = 20
     }
 
-
+    
+    @IBAction func showTrueButtonPressed() {
+        tomaLabel.isHidden.toggle()
+        
+        if tomaLabel.isHidden {
+            showTrueButton.setTitle("Show True", for: .normal)
+        } else {
+            showTrueButton.setTitle("Hide True", for: .normal)
+        }
+    }
 }
 
